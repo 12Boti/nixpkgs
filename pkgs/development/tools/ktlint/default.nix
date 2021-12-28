4fb7d4163eeee30643e9f6300c93acd867f8a486
Olle Boti <olle12boti+git@gmail.com>
2021-12-28 11:11:57 +0100

    ktlint: add formatting test

--- pkgs/development/tools/ktlint/default.nix
+++ pkgs/development/tools/ktlint/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, makeWrapper, jre_headless }:
+{ lib, stdenv, fetchurl, makeWrapper, jre_headless, runCommand, ktlint }:
 
 stdenv.mkDerivation rec {
   pname = "ktlint";
@@ -20,6 +20,19 @@ stdenv.mkDerivation rec {
   postFixup = ''
     wrapProgram $out/bin/ktlint --prefix PATH : "${jre_headless}/bin"
   '';
+
+  passthru.tests = {
+    format-file = runCommand "${pname}-format-file-test" { } ''
+      mkdir -p $out
+      f=$out/file.kt
+      cat > $f << EOF
+      fun main(args: Array<String>) {
+        println("Hello, World!")
+      }
+      EOF
+      ${ktlint}/bin/ktlint --format $f
+    '';
+  };
 
   meta = with lib; {
     description = "An anti-bikeshedding Kotlin linter with built-in formatter";
